9abfef88dcc28debbbbbee77785bc657957ad95d
Kacper Kubit <kacper.kubit@stazysta.comarch.pl>
2022-08-12 11:43:22 +0200

    added heroku

--- utils/db.ts
+++ utils/db.ts
@@ -1,8 +1,9 @@
 import {createPool} from "mysql2/promise";
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
+    host: 'eu-cdbr-west-03.cleardb.net',
+    user: 'bc2714eca589c8',
+    password: '424fe55d',
     database: 'med_app',
     namedPlaceholders: true,
     decimalNumbers: true,
